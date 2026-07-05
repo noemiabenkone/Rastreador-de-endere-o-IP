5a86b49e98d85d708ebf981ce3b3f244672db72d
Noêmia Benkone <noemiabenkone0@gmail.com>
2026-07-05 09:47:02 -0300

    feat: renderiza dados do card dinamicamente e integra coordenadas ao mapa

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
 import { ChevronRight } from "lucide-react";
 import MapComponent from "./components/MapComponent";
-import { useState } from "react";
+import {useState} from 'react';
 
 
 function App() {
-  const [searchIP, setSearchIP] = useState("")
-  const [ipData, setIPData] = useState<any>(null)
-  
-  const handleSearch = () => {
-    setSearchIP(searchIP.trim())
-    fetchIPData(searchIP.trim())
-  } 
-  
-  async function fetchIPData( ip: string) {
-    const response = await fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=at_rQ6jEdCITnOlUypv4YgHat1hnDt1v&ipAddress=${ip}`)
-    const data = await response.json()
-    setIPData(data)
-  }
+ const[searchIP, setSearchIP] = useState('')
+ const[ipData, setIPData] = useState<any>(null)
+
+ const handleSearch = () => {
+  setSearchIP(searchIP.trim())
+  getIPData(searchIP.trim())
+
+ }
+ async function getIPData(ip: string){
+  const response = await fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=at_rQ6jEdCITnOlUypv4YgHat1hnDt1v&ipAddress=${ip}`)
+  const data = await response.json()
+  setIPData(data)
+ }
 
  return (
   <main 
@@ -85,7 +85,7 @@ function App() {
             focus:outline-none"
         />
         <button 
-          onClick={handleSearch}
+         onClick={handleSearch}
           className="
             flex 
             items-center 
@@ -109,32 +109,160 @@ function App() {
         </button>
       </div>
 
-      <div className="absolute left-0 right-0 bottom-0 translate-y-1/2 w-full flex justify-center px-4 z-20">
-        <div className="w-full max-w-5xl bg-white rounded-2xl shadow-xl px-6 py-8">
-          <div className="grid grid-cols-1 md:grid-cols-4 gap-8 md:gap-0">
+      <div 
+        className="
+          absolute 
+          left-0 
+          right-0 
+          bottom-0 
+          translate-y-1/2 
+          w-full 
+          flex 
+          justify-center 
+          px-4 z-20">
+
+        <div 
+          className="
+           w-full 
+           max-w-5xl 
+           bg-white 
+           rounded-2xl 
+           shadow-xl 
+           px-6 py-8">
+
+          <div 
+            className="
+              grid 
+              grid-cols-1 
+              md:grid-cols-4 
+              gap-8 
+              md:gap-0">
             
-            <div className="flex flex-col items-center md:items-start md:border-r md:border-gray-300 md:pr-8">
-              <span className="text-xs font-semibold text-gray-500 uppercase tracking-widest mb-2">endereço IP</span>
-              <span className="text-xl md:text-2xl font-bold text-gray-800">
-                {ipData?.ip || "Buscando IP..."}
+            <div 
+              className="
+                flex 
+                flex-col 
+                items-center 
+                md:items-start 
+                md:border-r 
+                md:border-gray-300 
+                md:pr-8">
+
+              <span 
+                className="
+                text-xs 
+                font-semibold 
+                text-gray-500 
+                uppercase 
+                tracking-widest 
+                mb-2">
+                endereço IP
+              </span>
+
+              <span 
+                className="
+                  text-xl 
+                  md:text-2xl 
+                  font-bold 
+                  text-gray-800">
+                {ipData?.ip || "Buscando endereço IP..."}
               </span>
             </div>
 
-            <div className="flex flex-col items-center md:items-start md:border-r md:border-gray-300 md:px-8">
-              <span className="text-xs font-semibold text-gray-500 uppercase tracking-widest mb-2">Localização</span>
-              <span className="text-xl md:text-2xl font-bold text-gray-800 text-center md:text-left">
-                Brooklyn, NY <br className="hidden md:inline" /> 10001
+            <div 
+              className="
+                flex 
+                flex-col 
+                items-center 
+                md:items-start 
+                md:border-r 
+                md:border-gray-300 
+                md:px-8">
+
+              <span 
+                className="
+                  text-xs 
+                  font-semibold 
+                  text-gray-500 
+                  uppercase 
+                  tracking-widest 
+                  mb-2">
+                Localização
               </span>
+
+              <span 
+                className="
+                  text-xl 
+                  md:text-2xl 
+                  font-bold 
+                  text-gray-800 
+                  text-center 
+                  md:text-left">
+                {ipData ? `${ipData.location?.city}, ${ipData.location?.region}` : "Buscando..."} 
+                <br className="hidden md:inline" /> 10001
+              </span>
+
             </div>
 
-            <div className="flex flex-col items-center md:items-start md:border-r md:border-gray-300 md:px-8">
-              <span className="text-xs font-semibold text-gray-500 uppercase tracking-widest mb-2">Fuso horário</span>
-              <span className="text-xl md:text-2xl font-bold text-gray-800">UTC -05:00</span>
+            <div 
+              className="
+                flex 
+                flex-col 
+                items-center 
+                md:items-start 
+                md:border-r 
+                md:border-gray-300 
+                md:px-8">
+
+              <span 
+                className="
+                  text-xs 
+                  font-semibold 
+                  text-gray-500 
+                  uppercase 
+                  tracking-widest 
+                  mb-2">
+                Fuso horário
+              </span>
+
+              <span 
+                className="
+                  text-xl 
+                  md:text-2xl 
+                  font-bold 
+                  text-gray-800">
+                {ipData ? `UTC ${ipData.location?.timezone}` : "Buscando..."}
+              </span>
+
             </div>
 
-            <div className="flex flex-col items-center md:items-start md:pl-8">
-              <span className="text-xs font-semibold text-gray-500 uppercase tracking-widest mb-2">ISP</span>
-              <span className="text-xl md:text-2xl font-bold text-gray-800">SpaceX Starlink</span>
+            <div 
+              className="
+                flex 
+                flex-col 
+                items-center 
+                md:items-start 
+                md:pl-8">
+              <span 
+                className="
+                  text-xs 
+                  font-semibold 
+                  text-gray-500 
+                  uppercase 
+                  tracking-widest 
+                  mb-2">
+                ISP
+              </span>
+
+              <span 
+                className="
+                  text-xl 
+                  md:text-2xl 
+                  font-bold 
+                  text-gray-800">
+                {ipData?.isp || "Buscando..."}
+              </span>
+
             </div>
 
           </div>
@@ -143,9 +271,17 @@ function App() {
 
     </section>
 
-    
-    <section className="w-full flex-1 bg-gray-200 min-h-[400px] z-0">
-      <MapComponent lat={40.650002} lng={-73.949997} />
+    <section 
+      className="
+        w-full 
+        flex-1 
+        bg-gray-200 
+        min-h-[400px] 
+        z-0">
+      <MapComponent 
+       lat={ipData?.location?.lat || -23.55052} 
+       lng={ipData?.location?.lng || -46.633308} 
+      />
     </section>
    
   </main>
